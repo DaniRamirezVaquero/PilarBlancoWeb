a782db171a59d759f7e086997f55d1f185819f11
DaniRamirezVaquero <dramvaq@gmail.com>
2026-01-18 14:24:27 +0100

    Cambios 08/01/2026
    - CV Actualizado
    - Página de Bibliografía ahora es una imagen y descargable

--- src/app/Pages/bio-page/bio-page.component.ts
+++ src/app/Pages/bio-page/bio-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DownloadBioBtnComponent } from '../../components/download-bio-btn/download-bio-btn.component';
 
 @Component({
   selector: 'app-bio-page',
   standalone: true,
-  imports: [],
+  imports: [DownloadBioBtnComponent],
   templateUrl: './bio-page.component.html',
   styleUrl: './bio-page.component.css'
 })
