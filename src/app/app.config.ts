4feda757d2150bf4ffd379f03b27a520c1ad31a0
Daniel Ramirez <daniel.ramirez@aliqindoi.com>
2026-09-15 14:19:18 +0200

    Evitar el destelleo de estilos al cargar la página.
    Quita el import bloqueante de fuentes, muestra la app cuando ya está montada y salta la view transition inicial.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,11 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, withViewTransitions({ skipInitialTransition: true })),
     importProvidersFrom(RecaptchaV3Module),
     {provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.recaptcha.siteKey},
   ],
